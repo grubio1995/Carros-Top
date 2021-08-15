6d1ed4859a746d5e63b32958c706df89b8f142ed
Gabriel Rubio <gabrielpinhatarubio@gmail.com>
2021-08-14 22:40:40 -0300

    Layout X-Small

---
+++ frontend/src/components/Button/index.tsx
@@ -0,0 +1,11 @@
+import './styles.css';
+
+const Button = () => {
+  return (
+    <button className="btn">
+      <h1>VER CATÁLOGO</h1>
+    </button>
+  );
+};
+
+export default Button;
---
+++ frontend/src/pages/Home/index.tsx
@@ -0,0 +1,35 @@
+import Navbar from 'components/Navbar';
+import CarImg from 'assets/images/car.png';
+
+import './styles.css';
+import Button from 'components/Button';
+
+const Home = () => {
+  return (
+    <>
+      <Navbar />
+      <div className="home-container">
+        <div className="home-card">
+          <div className="home-content-container">
+            <h1>O carro perfeito para você</h1>
+            <p>
+              Conheça nossos carros e dê mais um passo na realização do seu
+              sonho
+            </p>
+          </div>
+          <div className="home-image-container">
+            <img src={CarImg} alt="Carro" />
+          </div>
+        </div>
+        <div className="home-card-bottom">
+          <div className="home-bottom-content-container">
+            <Button />
+            <p>Comece agora a navegar</p>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default Home;
